562af971a5b55b53d341699e4ca73ce1fb089cea
rauleflores <58542850+rauleflores@users.noreply.github.com>
2020-04-12 13:36:14 -0700

    added events to nav section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// Your code goes here
+//body
+const bodyVar = document.querySelector('body')
+console.log(bodyVar)
+// bodyVar.addEventListener('wheel', event => {
+//         event.target.style.backgroundColor = 'red'
+//     })
+
+//nav
+const anchors = document.querySelectorAll('a')
+let navChildren = anchors.children;
+console.log(anchors)
+anchors.forEach( links => {
+    links.style.color = 'red';
+    links.style.textDecoration = 'underline overline';
+    console.log(links)
+    links.addEventListener('mouseenter', (ev) => {
+      ev.target.style.fontSize = '24px';
+      ev.target.style.color = 'dodgerblue';
+    })
+    links.addEventListener('mouseleave', (ev) => {
+        ev.target.style.fontSize = 'initial';
+        ev.target.style.color = 'red';
+      })
+})
+
+
