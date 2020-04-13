db37463bf547d37c0862cf1cb85cf98be72ad2cd
rauleflores <58542850+rauleflores@users.noreply.github.com>
2020-04-13 16:08:14 -0700

    total 11 unique event listeners

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 //body
 const bodyVar = document.querySelector('body')
-//event listener 1
-console.log(bodyVar)
+//event listener 1 - wheel
 bodyVar.addEventListener('wheel', () => {
     var prevScrollPos = window.pageYOffset;
     window.onscroll = function() {
@@ -13,62 +12,88 @@ bodyVar.addEventListener('wheel', () => {
         }
     }     
 }, false)
-
 //nav
 const headerBar = document.querySelector('header')
-console.log(headerBar)
 headerBar.style.zIndex = '1';
 headerBar.style.transition = '.3s';
-//event listener 2
+//event listener 2 - mousemove
 headerBar.addEventListener('mousemove', () => {
     headerBar.style.backgroundColor = 'pink'
 })
+//event listener 3 - mouseleave
 headerBar.addEventListener('mouseleave', (ev) => {
     ev.target.style.backgroundColor = '';
 })
-
 const anchors = document.querySelectorAll('a')
-console.log(anchors)
 anchors.forEach( links => {
     links.style.color = 'red';
     links.style.textDecoration = 'underline overline';
-    console.log(links)
-    //event listener 3
+    //event listener 4 - mouseenter
     links.addEventListener('mouseenter', (ev) => {
       ev.target.style.fontSize = '24px';
       ev.target.style.color = 'dodgerblue';
       ev.target.style.transition = '1s'
     })
-    //event listener 4
     links.addEventListener('mouseleave', (ev) => {
         ev.target.style.fontSize = 'initial';
         ev.target.style.color = 'red';        
       })
-    //event listener 5
+    //event listener 5 - click
     links.addEventListener('click', (ev) => {
         ev.preventDefault();
     })
 })
-
 //main content
 let images = document.querySelectorAll('img')
-//event listener 6
-images[0].addEventListener('dblclick', (ev) => {
-    ev.target.style.transform = 'scale(1.5)';
-    ev.target.style.transition = '1s';
-    ev.target.style.opacity = '1'
-})
-images[0].addEventListener('mouseleave', (ev) => {
-    ev.target.style = 'initial'
-    ev.target.style.transition = '2s';
-})
 images.forEach( imgs => {
+    imgs.style.transition = 'all ease-in-out 500ms';
+    //event listener 6 - dblclick
+    imgs.addEventListener('dblclick', (ev) => {
+        ev.target.style.transform = 'scale(1.5)';        
+        ev.target.style.opacity = '1';
+        ev.target.style.border = '1px solid black'
+    })
     imgs.addEventListener('mouseenter', (ev) => {
         ev.target.style.opacity = '.7'
         ev.target.style.cursor = 'pointer'
-        ev.target.style.transition = '.5s'
+           })
+    imgs.addEventListener('mouseleave', (ev) => {        
+        ev.target.style = 'initial'
+        ev.target.style.transition = '2s';
     })
-    imgs.addEventListener('mouseleave', (ev) => {
-        ev.target.style.opacity = '1'
+})
+let mainContentH2 = document.querySelectorAll('.container h2')
+console.log(mainContentH2)
+mainContentH2.forEach( h => {
+    //event listener 7 - contextmenu
+    h.addEventListener('contextmenu', (ev) => {
+        ev.preventDefault();
     })
+    //event listener 8 - drag
+    h.addEventListener('drag', (ev) => {
+    console.log(ev.target)
+        h.style.opacity = '0'
+    })
+    //event listener 9 - dragend
+    h.addEventListener('dragend', () => {
+        h.style.opacity = '1'
+    })
+})
+let mainContentP = document.querySelectorAll('.container p')
+mainContentP.forEach( txt => {
+    txt.addEventListener('contextmenu', (ev) => {
+        ev.preventDefault();
+    })    
+})
+let columnPara = document.querySelectorAll('.content-pick .destination p')
+columnPara.forEach( p => {
+    //query selector 10 - pointerenter
+    p.addEventListener('pointerenter', (ev) => {        
+        p.style.textDecoration = 'underline'
+        //ev.stopPropagation();
+     })
+     //query selector 11 - pointerleave
+     p.addEventListener('pointerleave', (ev) => {
+        p.style.textDecoration = ''
+     })
 })
